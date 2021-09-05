d73f1e70d939834d85bea8400f60ecccf8a8e341
cht <3237126351@qq.com>
2021-09-05 23:09:00 +0800

    1.0 downloadbar

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,42 @@ pub mod config;
 pub mod core;
 pub mod download;
 pub mod utils;
-use gtk::{Box, Button, ProgressBar};
-struct DownloadProgressBar;
+use gtk::prelude::*;
+use download::*;
+use std::thread;
+use gtk::{Box, Button, ProgressBar, prelude::{BoxExt, ButtonExt}};
+pub struct DownloadProgressBar;
 impl DownloadProgressBar {
-    fn new() -> Box {
+    pub fn new(url: String) -> Box {
         let progress_bar = gtk::Box::new(gtk::Orientation::Horizontal, 0);
+        let download_bar = ProgressBar::new();
+        let download_button = Button::with_label("start");
+        progress_bar.pack_start(&download_bar, true, true, 0);
+        progress_bar.pack_start(&download_button, false, false, 0);
+        download_button.connect_clicked(move |button|{
+            let url = utils::parse_url(&url).unwrap();
+            let (tx, rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
+            thread::spawn(move || match url.scheme() {
+                "ftp" => ftp_download(tx, url, false, None),
+                "http" | "https" => http_download(tx, url),
+                _ => utils::gen_error(format!("unsupported url scheme '{}'", url.scheme())),
+            });
+            button.hide();
+            rx.attach(None, glib::clone!(@weak download_bar, @weak button=> @default-return glib::Continue(false),move |value| match value{
+                Some(length)=>{
+                    download_bar.set_fraction(length);
+                    glib::Continue(true)
+                },
+                None => {
+                    println!("finish");
+                    button.show();
+                    button.set_label("Finish");
+                    glib::Continue(false)
+                }
+            }));
+
+
+        });
         progress_bar
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 use std::process;
 
 use anyhow::Result;
-use duma::download::{ftp_download, http_download};
-use duma::utils;
 use gtk::prelude::*;
-use std::thread;
 fn main() {
     let application = gtk::Application::new(Some("com.ssss"), gio::ApplicationFlags::HANDLES_OPEN);
     application.connect_open(|window, _, _| match run(window) {
@@ -28,27 +25,9 @@ fn run(application: &gtk::Application) -> Result<()> {
     let window = gtk::ApplicationWindow::new(application);
     window.set_title("Accessibility");
     window.set_position(gtk::WindowPosition::Center);
-    let url = utils::parse_url(
-        "https://d.store.deepinos.org.cn//store/chat/chaoxin/chaoxin_1.8.3_amd64.deb",
-    )?;
-    let progress_bar = gtk::ProgressBar::new();
-    let (tx, rx) = glib::MainContext::channel(glib::PRIORITY_DEFAULT);
-    thread::spawn(move || match url.scheme() {
-        "ftp" => ftp_download(tx, url, false, None),
-        "http" | "https" => http_download(tx, url),
-        _ => utils::gen_error(format!("unsupported url scheme '{}'", url.scheme())),
-    });
-
-    rx.attach(None, glib::clone!(@weak progress_bar=> @default-return glib::Continue(false),move |value| match value{
-        Some(length)=>{
-            progress_bar.set_fraction(length);
-            glib::Continue(true)
-        },
-        None => {
-            println!("finish");
-            glib::Continue(false)
-        }
-    }));
+    let progress_bar = duma::DownloadProgressBar::new(
+        "https://d.store.deepinos.org.cn//store/chat/chaoxin/chaoxin_1.8.3_amd64.deb".to_string(),
+    );
     window.add(&progress_bar);
     window.show_all();
 
